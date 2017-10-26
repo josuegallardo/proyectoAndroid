24c3b28c45a691aec62954fe72cdda140f1eda33
Josue Gallardo <josuegallardor@gmail.com>
2017-10-26 10:51:48 -0500

    Update menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+//Menu responsive
+$("#icon-mobile").click(menu)
+$(".menuMobile a").click(menuCerrar)
+$(".cerrar").click(menuCerrar)
+function menu(){
+  $(".menuMobile").slideToggle();
+  $("#icon-mobile").hide();
+}
+function menuCerrar(){
+  $(".menuMobile").hide();
+  $("#icon-mobile").show();
+}
+//Slider de imagenes
 var slideIndex = 1;
 showDivs(slideIndex);
 var myIndex = 0;
@@ -5,15 +18,13 @@ carousel();
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
-
 function currentDiv(n) {
   showDivs(slideIndex = n);
 }
-
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("cajaSlide");
+  var x = $(".slides");
+  var dots = $(".cajaSlide");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
@@ -25,14 +36,20 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " goldSlider";
 }
+//Carrusel de fotos
 function carousel() {
-    var i;
-    var x = document.getElementsByClassName("slides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 5000); // Carga cada 5 segundos
+  var i;
+  var x = $(".slides");
+  var dots = $(".cajaSlide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" goldSlider", "");
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";
+  dots[myIndex-1].className += " goldSlider";
+  setTimeout(carousel, 5000); // Carga cada 5 segundos
 }
